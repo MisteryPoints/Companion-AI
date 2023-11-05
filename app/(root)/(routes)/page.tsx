025366d5992349b85dbf44d75d6b7c49d0b9829e
MisteryPoints <misterypoint2@gmail.com>
2023-11-04 23:30:42 -0400

    adding categories, prismadb and ending sidebar

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,9 +1,14 @@
-import { UserButton } from '@clerk/nextjs'
+import Categories from '@/components/Categories'
+import SearchInput from '@/components/SearchInput'
+import prismadb from '@/lib/prismadb'
 import React from 'react'
 
-const RootPage = () => {
+const RootPage = async () => {
+  const categories = await prismadb.category.findMany()
   return (
-    <div> 
+    <div className='h-full p-4 space-y-2'> 
+      <SearchInput />
+      <Categories data={categories} />
     </div>
   )
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,6 +8,7 @@ import { Menu, Sparkles } from 'lucide-react'
 import { Poppins } from 'next/font/google'
 import Link from 'next/link'
 import React from 'react'
+import MobileSidebar from './MobileSidebar'
 
 const font = Poppins({
     weight: '600',
@@ -18,7 +19,7 @@ const Navbar = () => {
   return (
     <div className='fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary h-16'>
         <div className='flex items-center'>
-            <Menu className="block md:hidden" />
+            <MobileSidebar />
             <Link href='/'>
                 <h1 className={cn('hidden md:block text-xl md:text-3xl font-bold text-primary', font.className)}>
                     companion.ai
